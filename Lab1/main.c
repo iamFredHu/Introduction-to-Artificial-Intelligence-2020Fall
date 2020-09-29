ab86a8f227ea2f68a5ccf29392a883bf5f5a9237
NoobCong <hucong@outlook.com>
2020-09-30 00:00:28 +0800

    create
    create new project

---
+++ Lab1/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void){
+    struct state{
+        int locMonkey; // 值为1-A 2-B 3-C
+        int locBox; //值为1-A 2-B 3-C
+        int locBanana; //值为1-A 2-B 3-C
+        int onBox; //值为0 不在盒子上 值为1 在盒子上
+    };
+    
+    
+}
