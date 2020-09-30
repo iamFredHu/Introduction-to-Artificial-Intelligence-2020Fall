74ff53a151b6f14d6a138e0fc6bdd7061ca9d5a7
NoobCong <hucong@outlook.com>
2020-09-30 08:43:49 +0800

    version 1

--- Lab1/main.c
+++ Lab1/main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 int main(void){
     struct state{
-        int locMonkey; // 值为1-A 2-B 3-C
-        int locBox; //值为1-A 2-B 3-C
-        int locBanana; //值为1-A 2-B 3-C
+        int atMonkey; // 值为1-A 2-B 3-C
+        int atBox; //值为1-A 2-B 3-C
+        int atBanana; //值为1-A 2-B 3-C
         int onBox; //值为0 不在盒子上 值为1 在盒子上
+        int holdBanana; //值为0 没有拿住香蕉 值为1 拿住香蕉
     };
     
     
